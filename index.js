b93fe12ed39e967835aa3343277ff873cbb5f2b6
Willem Veelenturf <willem.veelenturf@gmail.com>
2016-01-04 23:00:55 +0100

    update version

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 module.exports = {
-
-
-    core: {
-        ActorSystem: require("actorjs-core").ActorSystem,
-        ActorMatchers: require("actorjs-core").ActorMatchers,
-        ActorMessages: require("actorjs-core").ActorMessages
-    },
-    bus: {
-        EventBus: require("actorjs-bus").EventBus
-    },
-    persist: {
-        PersistStorage: require("actorjs-persist").PersistStorage,
-        PersistXMLHttp: require("actorjs-persist").PersistXMLHttp
-    },
-    components: {
-        InputComponent: require("./src/InputComponent"),
-        SelectComponent: require("./src/SelectComponent"),
-        ValidationComponent: require("./src/ValidationComponent")
-    }
-
+    InputComponent: require("./src/InputComponent"),
+    SelectComponent: require("./src/SelectComponent"),
+    ValidationComponent: require("./src/ValidationComponent")
 }
